6281ab7e2ffe6d68b7e02a63e9553bc8e30daf2f
riccardocloud <124076117+riccardocloud@users.noreply.github.com>
2024-10-10 10:56:46 +0200

    Update playlist_visualiser.py

--- playlist_visualiser.py
+++ playlist_visualiser.py
@@ -5,11 +5,12 @@ import plotly.io as pio
 import base64
 import pyfiglet
 import re
+import creds
 
 
 # Global variables
-client_id = 'df1e2b4953fb469291e7211b63afc6db'
-client_secret = 'db77c4ade47d4cc6aba9d740f60cae78'
+client_id = creds.client_id
+client_secret = creds.client_secret
 
 def authenitcate(id,secret):
     encoded_credentials = base64.b64encode(f"{id}:{secret}".encode()).decode()
